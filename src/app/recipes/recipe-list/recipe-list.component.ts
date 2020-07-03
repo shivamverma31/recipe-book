b791fa9981673b0ff11c8a7cd59653571ee265b8
Shivam <shivamverma31@gmail.com>
2020-07-03 17:35:09 +0530

    First Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  secretCheck:boolean;
+  countLog:any[];
+  secretMsg:string;
+
+  recipes: Recipe[]= [
+    new Recipe('Test', 'This is a test','https://th.bing.com/th/id/OIP.-703v8PMVC2RjgzIxd6usgHaE7?pid=Api&rs=1'),
+    new Recipe('Test', 'This is a test','https://th.bing.com/th/id/OIP.-703v8PMVC2RjgzIxd6usgHaE7?pid=Api&rs=1')];
+
+  constructor() {
+    this.secretCheck=false;
+    this.countLog=[];
+    this.secretMsg='Secret code = tuna';
+   }
+  chkClick(){
+    this.countLog.push(new Date());
+    this.secretCheck = !this.secretCheck;
+  }
+
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent{
+username:string='';
+onClick(){
+  this.username='';
+}
+}
+
+
