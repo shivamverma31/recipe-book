7c18d18fbb803190e8cc281a83b69669051b3afe
Shivam <shivamverma31@gmail.com>
2020-07-05 00:42:04 +0530

    New branch changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 
@@ -8,26 +8,22 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  // secretCheck:boolean;
-  // countLog:any[];
-  // secretMsg:string;
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[]= [
-    new Recipe('Test', 'This is a test','https://th.bing.com/th/id/OIP.-703v8PMVC2RjgzIxd6usgHaE7?pid=Api&rs=1'),
-    new Recipe('Test', 'This is a test','https://th.bing.com/th/id/OIP.-703v8PMVC2RjgzIxd6usgHaE7?pid=Api&rs=1')];
+    new Recipe('Recipe 1', 'description 1','https://th.bing.com/th/id/OIP.-703v8PMVC2RjgzIxd6usgHaE7?pid=Api&rs=1'),
+    new Recipe('Recipe 2', 'description 2','https://th.bing.com/th/id/OIP.-703v8PMVC2RjgzIxd6usgHaE7?pid=Api&rs=1')];
 
   constructor() {
-    // this.secretCheck=false;
-    // this.countLog=[];
-    // this.secretMsg='Secret code = tuna';
+
    }
-  // chkClick(){
-  //   this.countLog.push(new Date());
-  //   this.secretCheck = !this.secretCheck;
-  // }
+
 
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
