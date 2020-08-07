1d823e660356b09d280e8c844e14c714474a452e
Shivam <shivamverma31@gmail.com>
2020-08-08 04:23:58 +0530

    RoutingAdded

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -28,8 +28,12 @@ export class RecipeService{
 
     constructor(private slService:ShoppingListService){}
 
-   getRecipe(){
+   getRecipes(){
      return this.recipes.slice();
+   }
+
+   getRecipe(index:number){
+    return this.recipes[index];
    }
 
    addToShoppingList(ingredients:Ingredient[]){
